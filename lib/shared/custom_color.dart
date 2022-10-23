2827a240dfd8080330b302c1a65c8666943d4f05
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-23 23:31:06 +0900

    🍱 color set

--- lib/shared/custom_color.dart
+++ lib/shared/custom_color.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
 
-Map<String, Color> customColor = {
-  'black': const Color(0xFF1D1E1F),
-  'yellow': const Color(0xFFFFFF7E),
+enum CustomColor {
+  black,
+  yellow,
+  white,
+  grey,
+  green,
+  blue,
+  darkGreen,
+  crimson,
+  purple
+}
+
+Map<CustomColor, Color> customColor = {
+  CustomColor.black: const Color(0xFF1D1E1F),
+  CustomColor.yellow: const Color(0xFFFFFF7E),
+  CustomColor.white: const Color(0xFFFFFFFF),
+  CustomColor.grey: const Color(0xFF6F7276),
+  CustomColor.green: const Color(0xFF07BD82),
+  CustomColor.blue: const Color(0xFF3F8FF2),
+  CustomColor.darkGreen: const Color(0xFF73CD91),
+  CustomColor.crimson: const Color(0xFFFF5A5F),
+  CustomColor.purple: const Color(0xFF8D79FE)
 };
