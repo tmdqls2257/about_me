c1b2a53737e4b7cb2b5333dfc8e39a27bcb17e5d
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-27 00:21:27 +0900

    💄 bottom sheet scroll

--- lib/landingPage/widget/aboutMe.dart
+++ lib/landingPage/widget/aboutMe.dart
@@ -9,32 +9,65 @@ class AboutMe extends StatelessWidget {
   }) : super(key: key);
 
   final BuildContext context;
-
+  // var widgetKey = GlobalKey();
   void _showButtomSheet(BuildContext context) {
     showModalBottomSheet<void>(
+      isScrollControlled: true,
       context: context,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20.0),
       ),
       builder: (BuildContext context) {
-        return Container(
-          padding: const EdgeInsets.all(20),
+        return SizedBox(
           height: 462,
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    CloseButton(
-                      onPressed: () => Navigator.pop(context),
-                    ),
-                  ],
+                CloseButton(
+                  onPressed: () => Navigator.pop(context),
                 ),
               ],
             ),
-          ),
+            SizedBox(
+              height: 400,
+              child: Scrollbar(
+                thumbVisibility: true,
+                thickness: 3,
+                radius: const Radius.circular(2),
+                child: ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                    CustomText(text: 'sdsds'),
+                  ],
+                ),
+              ),
+            ),
+          ]),
         );
       },
     );
