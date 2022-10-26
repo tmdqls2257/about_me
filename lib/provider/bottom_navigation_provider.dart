72278e419255693151b1c5c49e041ebde4ce0a8e
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-26 16:43:31 +0900

    ✨ navigator push & pop

--- lib/provider/bottom_navigation_provider.dart
+++ lib/provider/bottom_navigation_provider.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class BottomNavigationProvider extends ChangeNotifier {
-  int _index = 0;
-  int get currentPage => _index;
-  updateCurrentPage(int index) {
-    _index = index;
+  // int _index = 0;
+  // int get currentPage => _index;
+  // updateCurrentPage(int index) {
+  //   _index = index;
+  //   notifyListeners();
+  // }
+  final List<int> _indexList = [0];
+  int get currentPage => _indexList[_indexList.length - 1];
+  push(int index) {
+    _indexList.add(index);
+    notifyListeners();
+  }
+
+  pop() {
+    _indexList.removeLast();
     notifyListeners();
   }
 }
