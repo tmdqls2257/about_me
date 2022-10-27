d921e06b50817e92604b453f0e84dba806dc4ab0
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-27 16:06:19 +0900

    💄 customCard

---
+++ lib/landingPage/widget/custom_card.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/shared.dart';
+
+class CustomCard extends StatelessWidget {
+  final Widget widget;
+
+  const CustomCard({super.key, required this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 178,
+      child: Card(
+        color: customColor[CustomColor.card],
+        child: widget,
+      ),
+    );
+  }
+}
