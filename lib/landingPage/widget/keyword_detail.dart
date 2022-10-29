c243716f53bddd7d1cc1c630cc31070f65659da3
tmdqls2257 <tmdqls2257@gmail.com>
2022-10-29 16:04:53 +0900

    🐛 overflow 92pixe,

--- lib/landingPage/widget/keyword_detail.dart
+++ lib/landingPage/widget/keyword_detail.dart
@@ -46,58 +46,60 @@ class KeywordDetail extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-            child: CustomScrollbar(listWidget: [
-              Container(
-                width: 46,
-                height: 20,
-                decoration: BoxDecoration(
-                  border: Border.all(color: customColor[CustomColor.blue]!),
-                ),
-                child: Center(
-                  child: CustomText(
-                    text: '키워드',
-                    color: CustomColor.blue,
-                    typoType: TypoType.labelLight,
+            child: CustomScrollbar(
+                height: constraints.maxHeight - 230,
+                listWidget: [
+                  Container(
+                    width: 46,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: customColor[CustomColor.blue]!),
+                    ),
+                    child: Center(
+                      child: CustomText(
+                        text: '키워드',
+                        color: CustomColor.blue,
+                        typoType: TypoType.labelLight,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 8,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomText(
-                    text: currentKeyword.title,
-                    typoType: TypoType.labelBolder,
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomText(
+                        text: currentKeyword.title,
+                        typoType: TypoType.labelBolder,
+                      ),
+                      CustomText(
+                        text: '22년 9월 인증 (총 9회)',
+                        typoType: TypoType.labelTiny,
+                      ),
+                    ],
                   ),
                   CustomText(
-                    text: '22년 9월 인증 (총 9회)',
+                    text: '20년 11월생 엄마 노량진동',
                     typoType: TypoType.labelTiny,
                   ),
-                ],
-              ),
-              CustomText(
-                text: '20년 11월생 엄마 노량진동',
-                typoType: TypoType.labelTiny,
-              ),
-              const SizedBox(
-                height: 14,
-              ),
-              Divider(
-                thickness: 1,
-                color: customColor[CustomColor.black],
-                height: 1,
-              ),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomText(
-                textAlign: TextAlign.start,
-                text: currentKeyword.content,
-                typoType: TypoType.bodyLight,
-              )
-            ]),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  Divider(
+                    thickness: 1,
+                    color: customColor[CustomColor.black],
+                    height: 1,
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  CustomText(
+                    textAlign: TextAlign.start,
+                    text: currentKeyword.content,
+                    typoType: TypoType.bodyLight,
+                  )
+                ]),
           ),
         ]),
       ),
